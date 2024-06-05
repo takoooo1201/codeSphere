897d1016f7d52178479bc1c4c509d4076e7a49c9
frostbean <ylai88193@gmail.com>
2024-06-06 02:28:06 +0800

    update: redirect index to entrance

--- app.py
+++ app.py
@@ -32,6 +32,9 @@ def close_connection(exception):
     if db is not None:
         db.close()
 
+@app.route('/')
+def index():
+    return redirect('/entrance')
 
 @app.route('/entrance')
 def entrance():
